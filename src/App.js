5a1d67066e54d4c62d3616882b9d0a9ba2fada98
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2020-08-25 09:29:49 +1200

    Added react router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import React, {useState} from 'react';
 import Nav from './Nav';
 import cardContent from './cardContents.json';
 import ViewCardsPage from './pages/ViewCardsPage';
-import './app.css'
+import './app.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   const [flashcards, setFlashcards] = useState(cardContent);
   return (
     <div>
       <Nav />
+      
+      
       <div className="selected-page">
-        <ViewCardsPage flashcards={flashcards}/>
+        <Router>
+          <Switch>
+            <Route path="/add">
+                <div>Add</div>
+            </Route>
+            <Route path="/cards">
+                <div>cards</div>
+            </Route>
+            <Route path="/">
+                <div>home</div>
+            </Route>
+          </Switch>
+        </Router>
+        {/* <ViewCardsPage flashcards={flashcards}/> */}
       </div>
     </div>
     
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import Link from 'react-router'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <div className="navbar">
-            <h1>Flashcards!</h1>
-            <button>Add</button>
-            <button>Cards</button>
-            <button>Learn</button>
-        </div>
+        <Router>
+            <div className="navbar">
+                <nav>
+                    <ul>
+                        <li>
+                        <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                        <Link to="/add">Add</Link>
+                        </li>
+                        <li>
+                        <Link to="/cards">Cards</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/add">
+                        <div>Add</div>
+                    </Route>
+                    <Route path="/cards">
+                        <div>cards</div>
+                    </Route>
+                    <Route path="/">
+                        <div>home</div>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
